30d004405250c5333bde4b0fd04ef717c28ce12c
Yevhenii Chekanskiy <echekanskiy@gmail.com>
2023-08-26 15:45:13 +0300

    Get item images.

--- src/client/kotlin/hellozyemlya/factory/App.kt
+++ src/client/kotlin/hellozyemlya/factory/App.kt
@@ -1,24 +1,33 @@
 package hellozyemlya.factory
 
-import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.layout.*
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.yield
 import net.minecraft.client.MinecraftClient
 import net.minecraft.inventory.Inventory
+import net.minecraft.item.Item
+import org.jetbrains.skia.Image
 
+@Composable
+fun Icon(image: Image, rotation: Float) {
+    Image(image.toComposeImageBitmap(), "", Modifier.rotate(rotation))
+}
 @Composable
 fun Inventory(inventory: Inventory) {
     var update by remember { mutableStateOf(0L) }
@@ -30,11 +39,33 @@ fun Inventory(inventory: Inventory) {
 //            println("updated")
         }
     }
+    var currentRotation by remember { mutableStateOf(0f) }
+
+    val rotation = remember { Animatable(currentRotation) }
+
+    LaunchedEffect(Unit) {
+        rotation.animateTo(
+            targetValue = currentRotation + 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(3000, easing = LinearEasing),
+                repeatMode = RepeatMode.Restart
+            )
+        ) {
+            currentRotation = value
+        }
+    }
 
     key(update) {
         LazyColumn(modifier = Modifier.width(200.dp).fillMaxHeight()) {
             items(inventory.size()) {
-                Text("Item ${inventory.getStack(it)}")
+                val stack = inventory.getStack(it)
+                val item = stack.item
+                val image = ExampleModClient.itemToIcon[item]
+                if (image != null) {
+                    Icon(image, rotation.value)
+                } else {
+                    Text("Item $stack")
+                }
             }
         }
     }
@@ -63,7 +94,7 @@ fun App() {
         }
 
         Box(Modifier.weight(1f)) {
-            if(playerInventory != null) {
+            if (playerInventory != null) {
                 Inventory(playerInventory)
             }
 
--- src/client/kotlin/hellozyemlya/factory/ExampleModClient.kt
+++ src/client/kotlin/hellozyemlya/factory/ExampleModClient.kt
@@ -3,50 +3,62 @@ package hellozyemlya.factory
 import androidx.compose.ui.ComposeScene
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Density
-import com.mojang.blaze3d.platform.GlConst.*
+import com.mojang.blaze3d.platform.GlConst
 import com.mojang.blaze3d.systems.RenderSystem
 import com.mojang.blaze3d.systems.VertexSorter
 import hellozyemlya.compose.ClientTickDispatcher
 import hellozyemlya.compose.MinecraftSurfaceManager
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gl.Framebuffer
 import net.minecraft.client.gl.SimpleFramebuffer
 import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.render.BackgroundRenderer
 import net.minecraft.client.render.DiffuseLighting
 import net.minecraft.client.texture.NativeImage
-import net.minecraft.client.util.ScreenshotRecorder
-import net.minecraft.item.Items
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import org.jetbrains.skia.Image
 import org.joml.Matrix4f
-import java.io.File
 
 
 object ExampleModClient : ClientModInitializer {
+    val itemToIcon = mutableMapOf<Item, Image>()
+
     val mainScene by lazy {
         ComposeScene(ClientTickDispatcher, density = Density(2f)).also { it.setContent { App() } }
     }
 
-    var rendered = false
     override fun onInitializeClient() {
         ClientTickDispatcher.setup()
 
-        HudRenderCallback.EVENT.register { ctx, delta ->
-//            MinecraftSurfaceManager.INSTANCE.render { canvas, surface ->
-//                mainScene.constraints = Constraints(maxWidth = surface.width, maxHeight = surface.height)
-//                mainScene.render(canvas, System.nanoTime())
-//            }
-            if(!rendered) {
-                rendered = true
-                val dimensions = 64
-                val fbo = SimpleFramebuffer(dimensions, dimensions, true, MinecraftClient.IS_SYSTEM_MAC)
-                fbo.setClearColor(0f, 1f, 0f, 0f)
+        ClientLifecycleEvents.CLIENT_STARTED.register {
+
+        }
+        ClientPlayConnectionEvents.JOIN.register { _, _, _ ->
+            println("generating images...")
+            val textureDim = 64
+            val fbo = SimpleFramebuffer(textureDim, textureDim, true, MinecraftClient.IS_SYSTEM_MAC)
+            fbo.setClearColor(0f, 1f, 0f, 0f)
+
+            Registries.ITEM.forEach {
                 fbo.clear(MinecraftClient.IS_SYSTEM_MAC)
+                RenderSystem.clear(
+                    GlConst.GL_DEPTH_BUFFER_BIT or GlConst.GL_COLOR_BUFFER_BIT,
+                    MinecraftClient.IS_SYSTEM_MAC
+                )
+
                 fbo.beginWrite(true)
+                BackgroundRenderer.clearFog()
+                RenderSystem.enableCull()
+                RenderSystem.clear(GlConst.GL_DEPTH_BUFFER_BIT, MinecraftClient.IS_SYSTEM_MAC)
                 val matrix4f = Matrix4f().setOrtho(
                     0.0f,
-                    dimensions.toFloat(),
-                    dimensions.toFloat(),
+                    textureDim.toFloat(),
+                    textureDim.toFloat(),
                     0.0f,
                     1000.0f,
                     21000.0f
@@ -56,28 +68,54 @@ object ExampleModClient : ClientModInitializer {
                 matrixStack.push()
                 matrixStack.loadIdentity()
                 matrixStack.translate(0.0f, 0.0f, -11000.0f)
-                matrixStack.scale((dimensions / 16).toFloat(), (dimensions / 16).toFloat(), 1f)
+                matrixStack.scale((textureDim / 16).toFloat(), (textureDim / 16).toFloat(), 1f)
                 RenderSystem.applyModelViewMatrix()
                 DiffuseLighting.enableGuiDepthLighting()
-                val drawContext = DrawContext(MinecraftClient.getInstance(), MinecraftClient.getInstance().bufferBuilders.entityVertexConsumers)
-                drawContext.drawItem(Items.DIAMOND.defaultStack, 0, 0)
+
+                val drawContext = DrawContext(
+                    MinecraftClient.getInstance(),
+                    MinecraftClient.getInstance().bufferBuilders.entityVertexConsumers
+                )
+
+                drawContext.drawItem(it.defaultStack, 0, 0)
+
                 drawContext.draw()
+                RenderSystem.clear(GlConst.GL_DEPTH_BUFFER_BIT, MinecraftClient.IS_SYSTEM_MAC)
+                matrixStack.pop()
+                RenderSystem.applyModelViewMatrix()
                 fbo.endWrite()
-                fbo.doScreenshot("hello.png")
-                fbo.delete()
-                rendered = true
+                itemToIcon[it] = fbo.toImage()
+            }
+            fbo.delete()
+            println("image generation finished")
+        }
+        HudRenderCallback.EVENT.register { ctx, delta ->
+            MinecraftSurfaceManager.INSTANCE.render { canvas, surface ->
+                mainScene.constraints = Constraints(maxWidth = surface.width, maxHeight = surface.height)
+                mainScene.render(canvas, System.nanoTime())
             }
         }
     }
 }
 
-fun Framebuffer.doScreenshot(filename: String) {
+//fun Framebuffer.doScreenshot(filename: String) {
+//    val i: Int = this.textureWidth
+//    val j: Int = this.textureHeight
+//    val nativeImage = NativeImage(i, j, false)
+//    RenderSystem.bindTexture(this.colorAttachment)
+//    nativeImage.loadFromTextureImage(0, false)
+//    nativeImage.mirrorVertically()
+//    nativeImage.writeTo(File(filename))
+//    nativeImage.close()
+//}
+
+fun Framebuffer.toImage(): Image {
     val i: Int = this.textureWidth
     val j: Int = this.textureHeight
-    val nativeImage = NativeImage(i, j, false)
-    RenderSystem.bindTexture(this.colorAttachment)
-    nativeImage.loadFromTextureImage(0, false)
-    nativeImage.mirrorVertically()
-    nativeImage.writeTo(File(filename))
-    nativeImage.close()
+    NativeImage(i, j, false).use { nativeImage ->
+        RenderSystem.bindTexture(this.colorAttachment)
+        nativeImage.loadFromTextureImage(0, false)
+        nativeImage.mirrorVertically()
+        return Image.makeFromEncoded(nativeImage.bytes)
+    }
 }
